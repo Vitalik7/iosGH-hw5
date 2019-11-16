110310e883350582f93ce2b4cdb0633be3b415a8
Vitalik7 <lavrenko.vitalik@yandex.ua>
2019-11-16 23:09:33 +0200

    Initial Commit

---
+++ hw5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw5
+//
+//  Created by Vitalik Lavrenko on 11/16/19.
+//  Copyright © 2019 Vitalik Lavrenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
